c9dde06937a7f758b0a83c197e5bbf62c5bda92a
Amit4218 <amitbhagat621@gmail.com>
2025-06-08 15:52:35 +0530

    fixed the errors i was getting

--- app/app_test/urls.py
+++ app/app_test/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.app_2_home, name="app_2_home")
+    path("app/",views.app_2_home, name="app_2_home")
 
 ]
--- app/app_test/views.py
+++ app/app_test/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def app_2_home(request):
-    return render(request, ("index.html"))
+    return render(request, ("app_test/index.html"))
